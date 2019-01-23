3bc6d0bbcd58f29ea5c3294899377598178ab5bc
affan-maajid <46916025+affan-maajid@users.noreply.github.com>
2019-01-23 16:50:48 +0800

    Add files via upload

---
+++ server0/app.js
@@ -0,0 +1,375 @@
+const random = require('random-js');
+const app = require('express')();
+const bodyParser = require('body-parser');
+const server = require('http').createServer(app);
+const ioBrowser = require('socket.io')(server);
+const io = require("socket.io").listen(8000);
+const io_client = require('socket.io-client');
+const SHA256 = require('crypto-js/sha256');
+const jsonSize = require('json-size');
+
+
+let serverWealth = [1,3,5];
+let availableServers = [1,0,0];
+let formData = {};
+let selectedValidator = -1;
+let tsuccess;
+
+let server1HasBeenAlive = false;
+let server2HasBeenAlive = false;
+
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use(bodyParser());
+
+app.post('/', function(req, res){
+    let From = req.body.from;
+    let To = req.body.to;
+    let Amount = req.body.amount;
+    let Description = req.body.desc;
+
+    formData = {from: From, to: To, amount: parseInt(Amount,10), description: Description};
+    console.log(formData);
+    selectedValidator = selectValidator(availableServers);
+    console.log('Selected Validator is: ' + selectedValidator);
+    passDataToSelectedValidator(selectedValidator);
+
+});
+
+
+const socket = io_client.connect('http://127.0.0.1:8001');
+
+socket.on('connect', function () {
+    socket.emit('server0Alive',1);
+    console.log('server 0 connected to server 1');
+    if (server1HasBeenAlive) {
+        socket.emit('updateYourBlockchain', {blockchain:server0Chain.chain});
+        console.log('Updated Blockchain sent to Server 1');
+        socket.emit('updateYourServerWealth', {wealtharray:serverWealth});
+        console.log('Updated Wealth array sent to Server 1');
+    }
+
+
+});
+
+socket.on('disconnect', function(){
+    //socket.close();
+    console.log('server 0 disconnected from server 1');
+    availableServers[1] = 0;
+    console.log(availableServers);
+    server1HasBeenAlive = true;
+});
+
+
+
+
+const osocket = io_client.connect('http://127.0.0.1:8002');
+
+osocket.on('connect', function () {
+    osocket.emit('server0Alive',1);
+    console.log('server 0 connected to server 2');
+    if (server2HasBeenAlive) {
+        osocket.emit('updateYourBlockchain', {blockchain:server0Chain.chain});
+        console.log('Updated Blockchain sent to Server 2');
+        osocket.emit('updateYourServerWealth', {wealtharray:serverWealth});
+        console.log('Updated Wealth array sent to Server 2');
+    }
+
+
+
+});
+
+osocket.on('disconnect', function(){
+    //osocket.close();
+    console.log('server 0 disconnected from server 2');
+    availableServers[2] = 0;
+    console.log(availableServers);
+    server2HasBeenAlive = true;
+});
+
+ioBrowser.on('connection', function(socket) {
+    console.log('Client opened browser window on Server 0');
+});
+
+
+io.on('connection', function(socket){
+    console.log('Another server has connected to Server 0');
+
+
+    socket.on('server1Alive', function(data){
+        availableServers[1] = data;
+        console.log('Current available servers: ' + availableServers);
+    });
+
+
+
+    socket.on('server2Alive', function(data){
+        availableServers[2] = data;
+        console.log('Current available servers: ' + availableServers);
+    });
+
+
+
+
+    socket.on('chosenValidator', function(data){
+        let currentBalance =  balanceOfClient(data.from);
+        formData = data;
+        requestStatus(currentBalance);
+
+    });
+
+
+    socket.on('updateYourBlockchain', function(data){
+        server0Chain.chain = [server0Chain.chain[0],server0Chain.chain[1],server0Chain.chain[2]];
+        for (let n = 3; n<data.blockchain.length; n++){
+            server0Chain.chain.push(data.blockchain[n]);
+        }
+        console.log('Server 0 blockchain is up to date');
+        console.log(JSON.stringify(server0Chain, null, 4));
+
+
+    });
+
+    socket.on('updateYourServerWealth', function(data){
+        serverWealth = data.wealtharray;
+        console.log('Server 0 wealth array is up to date');
+        console.log(serverWealth);
+
+    });
+
+
+
+    socket.on('transactionRejected', function(data){
+        ioBrowser.emit('formStatus', data);
+        //tsuccess = false;
+
+    });
+
+
+    socket.on('transactionAccepted', function(data){
+        ioBrowser.emit('formStatus', 'Transaction Accepted');
+        //tsuccess = true;
+        serverWealth[data.server] += 3;
+        console.log('Wealth of Validator Updated');
+
+        server0Chain.chain.push(data.newblock);
+        //addBlock(new Block(data.newblock.timeStamp, data.newblock.data, data.newblock.prevHash, data.newblock.hash));
+        console.log('Server 0 Block Chain Updated with New Block');
+        console.log(JSON.stringify(server0Chain, null, 4));
+
+    });
+
+
+
+
+    socket.on('disconnect', function(){
+        io.emit('aliveCheck',{server0:availableServers[0], server1:availableServers[1], server2:availableServers[2]});
+        //availableServers = [1,0,0];
+        //console.log('Current available servers: ' + availableServers);
+
+    });
+
+
+
+});
+
+
+
+class Block {
+    constructor(timeStamp, data, prevHash = "") {
+        this.timeStamp = Date.now();
+        this.data = data;
+        this.prevHash = prevHash;
+        this.hash = this.calculateHash();
+    }
+    calculateHash() {
+        return SHA256(this.prevHash + this.timeStamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+
+
+class BlockChain {
+    constructor() {
+// the chain is an array with an initial block (genesis block)
+        this.chain = [this.createGenesisBlock()];
+        //this.miningReward = 3;
+    }
+// create the first block. We call it the genesis block
+    createGenesisBlock() {
+        return new Block(Date.now(), {from:'', to:'', amount:0, description:''});
+    }
+
+// return the latest block
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+// this function creates a new block and adds it to the chain
+    addBlock(newBlock) {
+//assign the pre hash
+        newBlock.prevHash = this.getLatestBlock().hash;
+// Now calculate the hash of the new block
+        newBlock.hash = newBlock.calculateHash();
+// push it to the chain
+        this.chain.push(newBlock);
+    }
+
+
+
+}
+
+let server0Chain = new BlockChain();
+server0Chain.addBlock(new Block(Date.now(), {
+    from:-1, to:'C1', amount:100, description:'Open Balance C1'}));
+
+server0Chain.addBlock(new Block(Date.now(), {
+    from:-1, to:'C2', amount:150, description:'Open Balance C2'}));
+
+
+function balanceOfClient(from) {
+    let balance = 0;
+    server0Chain.chain.forEach(function(block) {
+        if (block.data.from === from){
+            balance -= block.data.amount;
+        }
+
+        if (block.data.to === from){
+            balance += block.data.amount;
+        }
+    });
+
+    return balance;
+}
+
+
+
+function requestStatus(balance) {
+  if (balance < formData.amount){
+      console.log('Transaction Rejected by Server 0');
+      //tsuccess = false;
+      ioBrowser.emit('formStatus', 'Transaction Rejected');
+      socket.emit('transactionRejected', 'Transaction Rejected');
+      osocket.emit('transactionRejected', 'Transaction Rejected');
+
+  }
+
+  else {
+      server0Chain.addBlock(new Block(Date.now(), {
+          from:formData.from, to:formData.to, amount:formData.amount, description:formData.description
+      }))
+
+      serverWealth[0] += 3;
+      console.log('Transaction Accepted by Server 0');
+      //tsuccess = true;
+      console.log(JSON.stringify(server0Chain, null, 4));
+      ioBrowser.emit('formStatus', 'Transaction Accepted');
+      let sobj = {server:0, newblock: server0Chain.chain[server0Chain.chain.length - 1]};
+      console.log('Object(Blockchain block) size in bytes: ' + jsonSize(sobj).toString());
+      socket.emit('transactionAccepted', {server:0, newblock: server0Chain.chain[server0Chain.chain.length - 1]});
+      osocket.emit('transactionAccepted', {server:0, newblock: server0Chain.chain[server0Chain.chain.length - 1]});
+
+  }
+
+}
+
+
+
+function passDataToSelectedValidator(selectedValidator) {
+    if (selectedValidator != 0) {
+        console.log('Object(formData) size in bytes: ' + jsonSize(formData).toString());
+        if (selectedValidator === 1) {
+            socket.emit('chosenValidator', formData);
+        }
+        else {
+            osocket.emit('chosenValidator', formData);
+        }
+    }
+    else {
+        let currentBalance =  balanceOfClient(formData.from);
+        requestStatus(currentBalance);
+
+
+
+    }
+}
+
+
+
+
+
+function getSum(total, num) {
+    return total + num;
+}
+
+
+
+function selectValidator(aServers) {
+    let selectedValidator = getRandomServerPropWealth();
+
+    if (aServers[selectedValidator] === 0){
+        let end = false;
+
+        while (!end) {
+            selectedValidator = getRandomServerPropWealth();
+            if (aServers[selectedValidator] === 1){
+                end = true;
+                return selectedValidator;
+            }
+        }
+    }
+
+    else {
+        return selectedValidator;
+    }
+
+}
+
+
+
+function generateNatural(distribution, engine) {
+    return distribution(engine);
+}
+
+
+function getRandomServerPropWealth() {
+    let totalServerWealth = serverWealth.reduce(getSum);
+    let i = -1;
+
+    let engine = random.engines.mt19937().autoSeed();
+    let distribution = random.integer(1,totalServerWealth);
+    let rand = generateNatural(distribution, engine);
+    //console.log(rand);
+
+    while (rand>0) {
+        i += 1;
+        rand = rand - serverWealth[i];
+
+    }
+
+    return i
+}
+
+
+
+
+
+
+
+/*
+let x = 0
+for (let k=0; k<100; k++) {
+    x = selectValidator(availableServers)
+    console.log(x);
+    if (x>2){
+        print('error');
+    }
+}
+*/
+
+server.listen(3000, function() {
+    console.log("Listening on port 3000");
+});
---
+++ server2/app.js
@@ -0,0 +1,380 @@
+const random = require('random-js');
+const app = require('express')();
+const bodyParser = require('body-parser');
+const server = require('http').createServer(app);
+const ioBrowser = require('socket.io')(server);
+const io = require("socket.io").listen(8002);
+const io_client = require('socket.io-client');
+const SHA256 = require('crypto-js/sha256');
+const jsonSize = require('json-size');
+
+
+let serverWealth = [1,3,5];
+let availableServers = [0,0,1];
+let formData = {};
+let selectedValidator = -1;
+let tsuccess;
+
+let server0HasBeenAlive = false;
+let server1HasBeenAlive = false;
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use(bodyParser());
+
+app.post('/', function(req, res){
+    let From = req.body.from;
+    let To = req.body.to;
+    let Amount = req.body.amount;
+    let Description = req.body.desc;
+
+    formData = {from: From, to: To, amount: parseInt(Amount,10), description: Description};
+    console.log(formData);
+
+    selectedValidator = selectValidator(availableServers);
+    console.log('Selected Validator is: ' + selectedValidator);
+    passDataToSelectedValidator(selectedValidator);
+
+
+});
+
+
+const socket = io_client.connect('http://127.0.0.1:8001');
+
+socket.on('connect', function () {
+    socket.emit('server2Alive',1);
+    console.log('server 2 connected to server 1');
+
+    if (server1HasBeenAlive) {
+        socket.emit('updateYourBlockchain', {blockchain:server2Chain.chain});
+        console.log('Updated Blockchain sent to Server 1');
+        socket.emit('updateYourServerWealth', {wealtharray:serverWealth});
+        console.log('Updated Wealth array sent to Server 1');
+    }
+});
+
+socket.on('disconnect', function(){
+    //socket.close();
+    console.log('server 2 disconnected from server 1');
+    availableServers[1] = 0;
+    console.log(availableServers);
+    server1HasBeenAlive = true;
+
+
+});
+
+
+
+
+const osocket = io_client.connect('http://127.0.0.1:8000');
+
+osocket.on('connect', function () {
+    osocket.emit('server2Alive',1);
+    console.log('server 2 connected to server 0');
+
+
+    if (server0HasBeenAlive) {
+        osocket.emit('updateYourBlockchain', {blockchain:server2Chain.chain});
+        console.log('Updated Blockchain sent to Server 0');
+        osocket.emit('updateYourServerWealth', {wealtharray:serverWealth});
+        console.log('Updated Wealth array sent to Server 0');
+    }
+
+
+});
+
+osocket.on('disconnect', function(){
+    //osocket.close();
+    console.log('server 2 disconnected from server 0');
+    availableServers[0] = 0;
+    console.log(availableServers);
+
+    server0HasBeenAlive = true;
+});
+
+
+ioBrowser.on('connection', function(socket) {
+    console.log('Client opened browser window on Server 2');
+});
+
+
+
+io.on('connection', function(socket){
+    console.log('Another server has connected to Server 2');
+
+
+    socket.on('server1Alive', function(data){
+        availableServers[1] = data;
+        console.log('Current available servers: ' + availableServers);
+    });
+
+
+
+    socket.on('server0Alive', function(data){
+        availableServers[0] = data;
+        console.log('Current available servers: ' + availableServers);
+    });
+
+
+
+    socket.on('chosenValidator', function(data){
+        let currentBalance =  balanceOfClient(data.from);
+        formData = data;
+        requestStatus(currentBalance);
+
+    });
+
+
+    socket.on('updateYourBlockchain', function(data){
+        server2Chain.chain = [server2Chain.chain[0],server2Chain.chain[1],server2Chain.chain[2]];
+        for (let n = 3; n<data.blockchain.length; n++){
+            server2Chain.chain.push(data.blockchain[n]);
+        }
+        console.log('Server 2 blockchain is up to date');
+        console.log(JSON.stringify(server2Chain, null, 4));
+
+
+    });
+
+    socket.on('updateYourServerWealth', function(data){
+        serverWealth = data.wealtharray;
+        console.log('Server 2 wealth array is up to date');
+        console.log(serverWealth);
+
+    });
+
+
+
+    socket.on('transactionRejected', function(data){
+        ioBrowser.emit('formStatus', data);
+
+    });
+
+
+    socket.on('transactionAccepted', function(data){
+        ioBrowser.emit('formStatus', 'Transaction Accepted');
+        serverWealth[data.server] += 3;
+        console.log('Wealth of Validator Updated');
+
+        server2Chain.chain.push(data.newblock);
+        //addBlock(new Block(data.newblock.timeStamp, data.newblock.data, data.newblock.prevHash, data.newblock.hash));
+        console.log('Server 2 Block Chain Updated with New Block');
+        console.log(JSON.stringify(server2Chain, null, 4));
+
+    });
+
+
+
+
+
+
+    socket.on('disconnect', function(){
+        io.emit('aliveCheck',{server0:availableServers[0], server1:availableServers[1], server2:availableServers[2]});
+        //availableServers = [0,0,1];
+        //console.log('Current available servers: ' + availableServers);
+
+    });
+
+
+
+});
+
+
+
+
+
+
+
+class Block {
+    constructor(timeStamp, data, prevHash = "") {
+        this.timeStamp = Date.now();
+        this.data = data;
+        this.prevHash = prevHash;
+        this.hash = this.calculateHash();
+    }
+    calculateHash() {
+        return SHA256(this.prevHash + this.timeStamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+
+
+
+
+
+
+class BlockChain {
+    constructor() {
+// the chain is an array with an initial block (genesis block)
+        this.chain = [this.createGenesisBlock()];
+        //this.miningReward = 3;
+    }
+// create the first block. We call it the genesis block
+    createGenesisBlock() {
+        return new Block(Date.now(), {from:'', to:'', amount:0, description:''});
+    }
+
+// return the latest block
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+// this function creates a new block and adds it to the chain
+    addBlock(newBlock) {
+//assign the pre hash
+        newBlock.prevHash = this.getLatestBlock().hash;
+// Now calculate the hash of the new block
+        newBlock.hash = newBlock.calculateHash();
+// push it to the chain
+        this.chain.push(newBlock);
+    }
+
+
+
+}
+
+let server2Chain = new BlockChain();
+server2Chain.addBlock(new Block(Date.now(), {
+    from:-1, to:'C1', amount:100, description:'Open Balance C1'}));
+
+server2Chain.addBlock(new Block(Date.now(), {
+    from:-1, to:'C2', amount:150, description:'Open Balance C2'}));
+
+
+function balanceOfClient(from) {
+    let balance = 0;
+    server2Chain.chain.forEach(function(block) {
+        if (block.data.from === from){
+            balance -= block.data.amount;
+        }
+
+        if (block.data.to === from){
+            balance += block.data.amount;
+        }
+    });
+
+    return balance;
+}
+
+
+function requestStatus(balance) {
+    if (balance < formData.amount){
+        console.log('Transaction Rejected');
+        ioBrowser.emit('formStatus', 'Transaction Rejected');
+        socket.emit('transactionRejected', 'Transaction Rejected');
+        osocket.emit('transactionRejected', 'Transaction Rejected');
+
+    }
+
+    else {
+        server2Chain.addBlock(new Block(Date.now(), {
+            from:formData.from, to:formData.to, amount:formData.amount, description:formData.description
+        }))
+
+        serverWealth[2] += 3;
+        console.log('Transaction Accepted by Server 2');
+        console.log(JSON.stringify(server2Chain, null, 4));
+        ioBrowser.emit('formStatus', 'Transaction Accepted');
+        let sobj = {server:2, newblock: server2Chain.chain[server2Chain.chain.length - 1]};
+        console.log('Object(Blockchain block) size in bytes: ' + jsonSize(sobj).toString());
+
+        socket.emit('transactionAccepted', {server:2, newblock: server2Chain.chain[server2Chain.chain.length - 1]});
+        osocket.emit('transactionAccepted', {server:2, newblock: server2Chain.chain[server2Chain.chain.length - 1]});
+    }
+
+}
+
+
+
+
+function passDataToSelectedValidator(selectedValidator) {
+    if (selectedValidator != 2) {
+        console.log('Object(formData) size in bytes: ' + jsonSize(formData).toString());
+        if (selectedValidator === 1) {
+            socket.emit('chosenValidator', formData);
+        }
+        else {
+            osocket.emit('chosenValidator', formData);
+        }
+    }
+    else {
+        let currentBalance =  balanceOfClient(formData.from);
+        requestStatus(currentBalance);
+
+
+
+    }
+}
+
+
+
+
+
+function getSum(total, num) {
+    return total + num;
+}
+
+
+
+function selectValidator(aServers) {
+    let selectedValidator = getRandomServerPropWealth();
+
+    if (aServers[selectedValidator] === 0){
+        let end = false;
+
+        while (!end) {
+            selectedValidator = getRandomServerPropWealth();
+            if (aServers[selectedValidator] === 1){
+                end = true;
+                return selectedValidator;
+            }
+        }
+    }
+
+    else {
+        return selectedValidator;
+    }
+
+}
+
+
+
+function generateNatural(distribution, engine) {
+    return distribution(engine);
+}
+
+
+function getRandomServerPropWealth() {
+    let totalServerWealth = serverWealth.reduce(getSum);
+    let i = -1;
+
+    let engine = random.engines.mt19937().autoSeed();
+    let distribution = random.integer(1,totalServerWealth);
+    let rand = generateNatural(distribution, engine);
+    //console.log(rand);
+
+    while (rand>0) {
+        i += 1;
+        rand = rand - serverWealth[i];
+
+    }
+
+    return i
+}
+
+/*
+let x = 0
+for (let k=0; k<100; k++) {
+    x = selectValidator(availableServers)
+    console.log(x);
+    if (x>2){
+        print('error');
+    }
+}
+*/
+
+server.listen(3002, function() {
+    console.log("Listening on port 3002");
+});
